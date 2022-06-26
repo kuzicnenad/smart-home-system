a61ff3051653fc8d726694343272250c8772a4ea
kuzicnenad <kuzic_nenad@protonmail.com>
2022-06-26 19:18:21 +0200

    rebuilt first push

---
+++ src/main/java/com/smarthomesystem/smarthomesystem/SmartHomeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.smarthomesystem.smarthomesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smarthomesystem/smarthomesystem/SmartHomeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthomesystem.smarthomesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
